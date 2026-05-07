72d9c7faa4b30fafad954561fcf45189e81174ec
Mostafizur Ahmed <mostafizur.ahmed@jayamsolutions.com>
2026-05-07 17:25:57 +0530

    first commit

--- app/src/main/java/com/ahmed/airesumebuilder/MainActivity.kt
+++ app/src/main/java/com/ahmed/airesumebuilder/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ahmed.airesumebuilder.ui.theme.AIResumeBuilderTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +24,7 @@ class MainActivity : ComponentActivity() {
             AIResumeBuilderTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = null,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +34,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String?, modifier: Modifier = Modifier) {
+
+    Box(modifier
+        .statusBarsPadding()
+        .padding(40.dp)) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     AIResumeBuilderTheme {
-        Greeting("Android")
+        Greeting(null)
     }
 }
